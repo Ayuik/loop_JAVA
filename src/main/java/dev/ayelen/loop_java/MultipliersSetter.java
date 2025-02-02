63d460a51a9686beebe00ac04c93a29b3be88061
Ayuik <marcosayelen@gmail.com>
2025-02-02 10:25:19 +0100

    corrections. Still fails

--- src/main/java/dev/ayelen/loop_java/MultipliersSetter.java
+++ src/main/java/dev/ayelen/loop_java/MultipliersSetter.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 
 public class MultipliersSetter {
 
-    public List<Integrer> setMultipliers(){
-
+    public List<Integer> setMultipliers(){
+        List<Integer> multipliers = new ArrayList<>(); 
+        for (int i = 1; i <= 10; i++){
+            int multiplier = i;
+            multipliers.add(multiplier);
+        }
+        return  multipliers;     
+        };
     }
-
-}
--- src/test/java/dev/ayelen/loop_java/MultipliersSetterTest.java
+++ src/test/java/dev/ayelen/loop_java/MultipliersSetterTest.java
@@ -1,7 +1,9 @@
 package dev.ayelen.loop_java;
 
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class MultipliersSetterTest {
@@ -11,10 +13,9 @@ public class MultipliersSetterTest {
         //Given
         MultipliersSetter multipliersSetter = new MultipliersSetter();
         //When
-        List<Integrer> multipliers = multipliersSetter.setMultipliers();
+        List<Integer> multipliers = multipliersSetter.setMultipliers();
         //Then
         assertThat(multipliers, contains(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        
     }
     
 }
